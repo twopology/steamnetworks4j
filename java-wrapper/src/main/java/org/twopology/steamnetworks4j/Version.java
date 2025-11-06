f773f7248dcb9b44c30a2df188a92718b9c3a868
Anthony Ruffino <aruffino84@gmail.com>
2025-11-06 10:40:00 +0100

    Fixes for native bindings.

--- java-wrapper/src/main/java/org/twopology/steamnetworks4j/Version.java
+++ java-wrapper/src/main/java/org/twopology/steamnetworks4j/Version.java
@@ -2,7 +2,7 @@ package org.twopology.steamnetworks4j;
 
 public final class Version {
 
-	private static final String VERSION = "1.10.0-SNAPSHOT";
+	private static final String VERSION = "0.0.9";
 
 	public static String getVersion() {
 		return VERSION;

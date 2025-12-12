c7fef29081c92eac85c8e904df820f57279f1e53
Anthony Ruffino <aruffino84@gmail.com>
2025-12-13 00:50:05 +0100

    0.0.16-SNAPSHOT - revert back to Java 21

--- java-wrapper/src/main/java/org/twopology/steamnetworks4j/Version.java
+++ java-wrapper/src/main/java/org/twopology/steamnetworks4j/Version.java
@@ -2,7 +2,7 @@ package org.twopology.steamnetworks4j;
 
 public final class Version {
 
-	private static final String VERSION = "0.0.15-SNAPSHOT";
+	private static final String VERSION = "0.0.16-SNAPSHOT";
 
 	public static String getVersion() {
 		return VERSION;
